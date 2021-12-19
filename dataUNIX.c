5f715cfd1d9b80ead54037f748f6e364d9cadfc6
Yan Rocha <yanpimenta7@gmail.com>
2021-12-19 20:42:57 -0300

    Implementação do dataUNIX

--- dataUNIX.c
+++ dataUNIX.c
@@ -3,5 +3,32 @@
 // meses e dias) usando as funções de tempo da biblioteca “time”.
 
 #include <stdio.h>
-#include <math.h>
-#include <pthread.h>
+#include <time.h>
+
+int main() {
+    int dia, mes, ano;
+    time_t dataNascimento, dataAtual;
+
+    struct tm *data, *idade;
+
+    printf("Qual o dia em que você nasceu?\n");
+    scanf("%d", &dia);
+    printf("\nQual o mês que você nasceu?\n");
+    scanf("%d", &mes);
+    printf("\nQual o ano que você nasceu?\n");
+    scanf("%d", &ano);
+
+    time(&dataNascimento);
+    data = localtime(&dataNascimento);
+    data -> tm_mday = dia;
+    data -> tm_mon = mes-1;
+    data -> tm_year = ano-1830;
+    
+    dataNascimento = mktime(data);
+    dataAtual = time(NULL);
+
+    long int idadeSeg = dataAtual - dataNascimento;
+    idade = localtime(&idadeSeg);
+
+    printf("\nVocê tem %d anos, %d meses e %d dias.\n", idade -> tm_year, idade -> tm_mon, idade -> tm_mday);
+}
